0197e85b156a88db387256d8a61c92e08c742913
miaaaaaaaa1 <solomia1pochaievets@gamil.com>
2025-09-19 17:55:22 +0300

    create ua

---
+++ SportShopWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataAccess1;
+
+namespace SportShopWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SportShopDb db;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["SportShopDbConnection"].ConnectionString;
+
+            db = new SportShopDb(connectionString);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.Read_Get_All();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
